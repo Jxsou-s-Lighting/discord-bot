1352799dc807351ff3b8e2c0da6ca75f9c403523
Jxsou <josoucy@outlook.com>
2022-09-14 22:06:10 -0400

    added some role commands

---
+++ src/commands/tools/addrole.js
@@ -0,0 +1,32 @@
+const { SlashCommandBuilder, EmbedBuilder, PermissionFlagsBits } = require("discord.js");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("add")
+        .setDescription("Add a role to a user.")
+        .setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
+        .addUserOption((member) =>
+            member.setName("member").setDescription("member to mention").setRequired(true)
+        )
+        .addRoleOption((role) =>
+            role.setName("role").setDescription("role to add").setRequired(true)
+        ),
+    async execute(interaction, client) {
+        const user = interaction.options.getUser("member");
+        const product = interaction.options.getRole("role");
+
+        interaction.guild.members.cache.get(user.id).roles.add(product);
+
+        const embed = new EmbedBuilder()
+            .setAuthor({
+                name: "Jxsou's Lighting | Roles System",
+                iconURL: client.user.displayAvatarURL(),
+            })
+            .setDescription(`Successfully added the **${product}** role.`)
+            .setTimestamp(Date.now());
+
+        await interaction.reply({
+            embeds: [embed],
+        });
+    },
+};
