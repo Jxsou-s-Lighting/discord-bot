d86b0f0632ffea852df4c32d75b5603bade4f91d
Jxsou <josoucy@outlook.com>
2022-09-15 18:10:06 -0400

    adds customer role

--- src/commands/tools/addrole.js
+++ src/commands/tools/addrole.js
@@ -16,6 +16,9 @@ module.exports = {
         const product = interaction.options.getRole("role");
 
         interaction.guild.members.cache.get(user.id).roles.add(product);
+        interaction.guild.members.cache
+            .get(user.id)
+            .roles.add(interaction.guild.roles.cache.get("992430449949429771"));
 
         const embed = new EmbedBuilder()
             .setAuthor({
