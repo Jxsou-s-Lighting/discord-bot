f3cdf7065f9ec853b08d4ea4f853ba4f0a3357d2
Jxsou <josoucy@outlook.com>
2024-01-27 00:46:21 -0500

    updated verification system

--- src/events/server/verify.js
+++ src/events/server/verify.js
@@ -1,52 +1,54 @@
-const { EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require("discord.js");
-const { ids } = require("../../../config.json");
-
-const verifiedUsers = new Set();
-const welcomedInteractions = new Set();
+const { ActionRowBuilder, ButtonBuilder, ButtonStyle } = require("discord.js");
+const config = require("../../../config.json");
 
 module.exports = {
   name: "interactionCreate",
-  async execute(interaction) {
+  async execute(interaction, client) {
+    if (!interaction.isButton()) return;
+
     if (interaction.customId === "verification") {
-      // Check if the interaction has already been welcomed
-      if (welcomedInteractions.has(interaction.id)) {
-        return;
+      const member = interaction.member;
+
+      const { roles, channels, links } = config.ids;
+
+      try {
+        await member.roles.remove(roles.unverifiedID);
+
+        await member.roles.add(roles.memberID);
+
+        const welcomeChannel = await client.channels.fetch(channels.welcomeID);
+
+        const embed = {
+          author: {
+            name: member.user.tag,
+            icon_url: member.user.displayAvatarURL({ dynamic: true }),
+          },
+          title: "Welcome to Jxsou's Lighting!",
+          description: `Please check out our [Rules and Info](${links.rulesLink}) as well as our [Product List](${links.productLink}).`,
+          image: {
+            url: "https://i.postimg.cc/T3X5sYQk/welcome.png",
+          },
+        };
+
+        const row = new ActionRowBuilder().addComponents(
+          new ButtonBuilder()
+            .setURL("https://jxsou.lighting/roblox")
+            .setLabel("Roblox Group")
+            .setStyle(ButtonStyle.Link),
+          new ButtonBuilder().setURL("https://jxsou.lighting/hub").setLabel("Product Hub").setStyle(ButtonStyle.Link)
+        );
+
+        welcomeChannel.send({
+          embeds: [embed],
+          components: [row],
+          content: `${member}`,
+        });
+
+        interaction.reply({ content: "You have been verified.", ephemeral: true });
+      } catch (error) {
+        console.error("Error during verification:", error);
+        interaction.reply({ content: "Error during verification process.", ephemeral: true });
       }
-
-      welcomedInteractions.add(interaction.id);
-
-      if (verifiedUsers.has(interaction.user.id)) {
-        return;
-      }
-
-      verifiedUsers.add(interaction.user.id);
-
-      await interaction.member.roles.add(ids.roles.memberID);
-      await interaction.member.roles.remove(ids.roles.unverifiedID);
-
-      interaction.reply({
-        content: "You have been verified.",
-        ephemeral: true,
-      });
-
-      const embed = new EmbedBuilder()
-        .setAuthor({
-          name: `${interaction.member.user.tag}`,
-          iconURL: `${interaction.member.user.displayAvatarURL({ forceStatic: false })}`,
-        })
-        .setTitle("Welcome to Jxsou's Lighting!")
-        .setDescription(
-          `Please check out our [Rules and Info](${ids.links.rulesLink}) as well as our [Product List](${ids.links.productLink}).`
-        )
-        .setImage("https://i.postimg.cc/T3X5sYQk/welcome.png");
-
-      const row = new ActionRowBuilder().addComponents(
-        new ButtonBuilder().setURL("https://jxsou.lighting/roblox").setLabel("Roblox Group").setStyle(ButtonStyle.Link),
-        new ButtonBuilder().setURL("https://jxsou.lighting/hub").setLabel("Product Hub").setStyle(ButtonStyle.Link)
-      );
-
-      let welcomeChannel = interaction.guild.channels.cache.get(ids.channels.welcomeID);
-      welcomeChannel.send({ content: `${interaction.member}`, embeds: [embed], components: [row] });
     }
   },
 };
